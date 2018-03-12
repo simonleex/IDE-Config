93e166c9a7075f86bcd8842aab8e0a2f99bc19c2
xinmin.li <xinmin.li@grabtaxi.com>
2018-03-12 10:44:31 +0800

    vs code

---
+++ vscode-golang/setlink.sh
@@ -0,0 +1,7 @@
+path=$[0]
+rm -rf keybindings.json
+rm -rf settings.json
+ln -s  /Users/lixinmin/codebox/IDE-Config/vscode-golang/keybindings.json keybindings.json
+ln -s  /Users/lixinmin/codebox/IDE-Config/vscode-golang/settings.json   settings.json
+
+
